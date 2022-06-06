09a3b3f60af268b3fa4811400ef788bcc7041c62
Saddam Husein <saddambinhusein2@gmail.com>
2022-06-06 11:04:03 +0800

    Added viewset to profile api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')  # start DRF 3.9 version and later, base_name argument has been renamed in favor to basename.
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
