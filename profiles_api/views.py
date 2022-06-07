458966ffe4176b04cd9455553cfded751f50240d
Saddam Husein <saddambinhusein2@gmail.com>
2022-06-07 18:43:30 +0800

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 # from django.http import JsonResponse
 
 from .models import UserProfile
@@ -109,6 +111,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+
+class UserLoginApiView(ObtainAuthToken):
+    '''Handle creating user authentication tokens'''
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 """
 class UserProfileApiView(APIView):
